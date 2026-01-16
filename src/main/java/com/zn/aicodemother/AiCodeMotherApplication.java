c9eb79f20caaa4bbc3e1178dcb6b5522aaa9613b
15458 <1545840703@qq.com>
2026-01-16 21:43:15 +0800

    chore(pom.xml ，AiCodeMotherApplication ) 增加AOP切面编程依赖和注解

--- src/main/java/com/zn/aicodemother/AiCodeMotherApplication.java
+++ src/main/java/com/zn/aicodemother/AiCodeMotherApplication.java
@@ -2,8 +2,10 @@ package com.zn.aicodemother;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class AiCodeMotherApplication {
 
 	public static void main(String[] args) {
