44b64dccff671f4d9bb0ce0896c2a159234fcc62
15458 <1545840703@qq.com>
2026-01-30 17:06:22 +0800

    feat(redis) 新增Redis配置及序列化支持

--- src/main/java/com/zn/aicodemother/AiCodeMotherApplication.java
+++ src/main/java/com/zn/aicodemother/AiCodeMotherApplication.java
@@ -1,12 +1,13 @@
 package com.zn.aicodemother;
 
+import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStoreAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class})
 @EnableAspectJAutoProxy(exposeProxy = true)
 @MapperScan("com.zn.aicodemother.mapper")
 public class AiCodeMotherApplication {
