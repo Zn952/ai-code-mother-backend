69fecdddff10edf7907dc5c4eafca567d29b7a79
15458 <1545840703@qq.com>
2026-02-06 15:17:37 +0800

    feat(langgraph4j) 新增各个节点实现类，获取上下文bean工具类

--- src/main/java/com/zn/aicodemother/langgraph4j/node/ImageCollectorNode.java
+++ src/main/java/com/zn/aicodemother/langgraph4j/node/ImageCollectorNode.java
@@ -1,8 +1,10 @@
 package com.zn.aicodemother.langgraph4j.node;
 
+import com.zn.aicodemother.langgraph4j.ai.ImageCollectionService;
 import com.zn.aicodemother.langgraph4j.model.ImageResource;
 import com.zn.aicodemother.langgraph4j.model.enums.ImageCategoryEnum;
 import com.zn.aicodemother.langgraph4j.state.WorkflowContext;
+import com.zn.aicodemother.utils.SpringContextUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.bsc.langgraph4j.action.AsyncNodeAction;
 import org.bsc.langgraph4j.prebuilt.MessagesState;
@@ -20,32 +22,26 @@ import static org.bsc.langgraph4j.action.AsyncNodeAction.node_async;
  **/
 @Slf4j
 public class ImageCollectorNode {
+
     public static AsyncNodeAction<MessagesState<String>> create() {
         return node_async(state -> {
             WorkflowContext context = WorkflowContext.getContext(state);
-            log.info("执行节点: 图片收集");
-
-            // TODO: 实际执行图片收集逻辑
-
-            // 简单的假数据
-            List<ImageResource> imageList = Arrays.asList(
-                    ImageResource.builder()
-                            .category(ImageCategoryEnum.CONTENT)
-                            .description("假数据图片1")
-                            .url("https://www.codefather.cn/logo.png")
-                            .build(),
-                    ImageResource.builder()
-                            .category(ImageCategoryEnum.LOGO)
-                            .description("假数据图片2")
-                            .url("https://www.codefather.cn/logo.png")
-                            .build()
-            );
-
+            String originalPrompt = context.getOriginalPrompt();
+            String imageListStr = "";
+            try {
+                // 获取AI图片收集服务
+                ImageCollectionService imageCollectionService = SpringContextUtil.getBean(ImageCollectionService.class);
+                // 使用 AI 服务进行智能图片收集
+                imageListStr = imageCollectionService.collectImages(originalPrompt);
+//                imageCollectionService.collectImages(originalPrompt);
+            } catch (Exception e) {
+                log.error("图片收集失败: {}", e.getMessage(), e);
+            }
             // 更新状态
             context.setCurrentStep("图片收集");
-            context.setImageList(imageList);
-            log.info("图片收集完成，共收集 {} 张图片", imageList.size());
+            context.setImageListStr(imageListStr);
             return WorkflowContext.saveContext(context);
         });
     }
 }
+
